9fba2cf44e4805ef673dcb79960bc6793ec42c6a
ste <stefaneadnas@gmail.com>
2022-05-10 10:26:21 -0300

    atividades da aula 01

---
+++ atividades/Aula-30-04/NLP/Aula_1/code2.py
@@ -0,0 +1,19 @@
+# Import necessary modules
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nlp_utils import get_sample_Santo_Graal
+import nltk 
+
+nltk.download('punkt')
+
+# Split scene_one into sentences: sentences
+scene_one = get_sample_Santo_Graal()
+sentences = sent_tokenize(scene_one)
+
+# Use word_tokenize to tokenize the fourth sentence: tokenized_sent
+tokenized_sent = word_tokenize(sentences[3])
+
+# Make a set of unique tokens in the entire scene: unique_tokens
+unique_tokens = set(word_tokenize(scene_one))
+
+# Print the unique tokens result
+print(unique_tokens)
